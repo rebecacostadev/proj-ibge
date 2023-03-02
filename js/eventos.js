759dbeb2b78130cb4deacc52dba4369b47906905
rebeca <rebecacosta.developer@gmail.com>
2023-03-02 11:49:13 -0300

    IBGE

--- js/eventos.js
+++ js/eventos.js
@@ -1,8 +1,12 @@
+document.querySelector(".bloqueio").classList.add("aparece");
+
 fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes`)
     .then( res => res.json())
     .then(regioes => {
+        document.querySelector(".bloqueio").classList.remove("aparece");
         function addOption(cada) {
             select_regiao.innerHTML += `<option value="${cada.id}">${cada.nome}</option>`
+            
         }
 
         regioes.forEach(addOption);
@@ -11,9 +15,12 @@ fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes`)
 
     function buscarEstados() {
 
+        document.querySelector(".bloqueio").classList.add("aparece");
+
         fetch(`https://servicodados.ibge.gov.br/api/v1/localidades//regioes/${select_regiao.value}/estados`)
             .then( res => res.json())
             .then(estados => {
+                document.querySelector(".bloqueio").classList.remove("aparece");
         select_estados.innerHTML = "<option> -- Selecione --</option>"
         select_cidades.innerHTML = "<option> -- Selecione --</option>"
         function addOption(cada) {
@@ -26,10 +33,13 @@ fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes`)
 
     
     function buscarCidades() {
-
+        document.querySelector(".bloqueio").classList.add("aparece");
+        
         fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${select_estados.value}/municipios`)
             .then( res => res.json())
             .then(cidades => {
+                document.querySelector(".bloqueio").classList.remove("aparece");
+
         select_cidades.innerHTML = "<option> -- Selecione --</option>"
 
         function addOption(cada) {
